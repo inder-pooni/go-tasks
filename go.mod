74b9c828eeff828e33db60913366ea885aa3a3bd
Inder Pooni <inder@inders-mbp.lan>
2021-09-17 22:42:56 -0700

    playing around with echo library

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/inder-pooni/go-tasks
 
 go 1.16
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/labstack/echo/v4 v4.5.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,71 @@
 package main
 
-func main() {
-	println("hello world")
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"log"
+	"net/http"
+)
+// data structure for representing user resource
+type user struct {
+	ID string `json:"id,omitempty"`
+	FirstName string `json:"firstName"`
+	LastName string `json:"lastName"`
+	Age int `json:"age"`
 }
+
+var users = map[string]*user{}
+
+func getUsers(ctx echo.Context) error {
+	userSlice := []user{}
+
+	for _ , v := range users {
+		userSlice = append(userSlice, *v)
+	}
+	return ctx.JSON(http.StatusOK , userSlice)
+}
+
+func getUserById(ctx echo.Context) error {
+	id := ctx.Param("id")
+	user := users[id]
+
+	if user == nil {
+		errMsg := fmt.Sprintf("Cound not find user with id: %s", id)
+		return ctx.JSON(http.StatusNotFound, errMsg)
+	}
+
+	return ctx.JSON(http.StatusOK , user)
+}
+
+func createUser(ctx echo.Context) error {
+	user := user{}
+	defer ctx.Request().Body.Close()
+	err := json.NewDecoder(ctx.Request().Body).Decode(&user)
+
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, "Bad Request")
+	}
+	user.ID = uuid.New().String()
+	log.Print(&user)
+
+	users[user.ID] = &user
+
+	log.Print("Successfully added new User")
+	return ctx.JSON(http.StatusOK, &user)
+}
+// echo context represents the context of the currect http request
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/users", getUsers)
+	e.POST("/users", createUser)
+	e.GET("/user/:id", getUserById)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+// endpoints: get users , post user , get user {id}
